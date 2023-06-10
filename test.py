063505f9ed5a9de409f60cdf222b41bc0b666652
Nina Mainusch <nina.mainusch@protonmail.com>
2023-06-10 14:49:25 +0200

    add test for setting parameters

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from main import LinearLayer
 
+
 class LinearNetwork():
     def __init__(self) -> None:
         self.linear_layer = LinearLayer(2, 1)
@@ -13,9 +14,10 @@ class LinearNetwork():
         x = self.linear_layer(x)
         return x
 
+
 class TestModules(unittest.TestCase):
 
-    def test_forward_pass(self):
+    def test_forward_pass_succeeds_for_2_x_1_network(self):
         """
         We are performing one forward step and basically doing the calculation:
         y = x * w.T + b
@@ -23,14 +25,17 @@ class TestModules(unittest.TestCase):
         y = [3]
         """
         network = LinearNetwork()
-        network.linear_layer.set_parameters(np.array([[1,2]]), np.array([[1]]))
-        data = np.array([[0,1]])
-
+        network.linear_layer.set_parameters(
+            np.array([[1, 2]]), np.array([[1]]))
+        data = np.array([[0, 1]])
         result = network.forward(data)
-
         self.assertTrue(np.array_equal(result, np.array([[3]])))
 
-
+    def test_setting_parameters_of_wrong_shape_fails(self):
+        network = LinearNetwork()
+        with self.assertRaises(AssertionError):
+            network.linear_layer.set_parameters(
+                np.array([[1]]), np.array([[1, 2]]))
 
 
 if __name__ == "__main__":
