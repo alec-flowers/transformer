7f270e2cc32d4021d1f4919dbb8591b4ded68101
Nina Mainusch <nina.mainusch@protonmail.com>
2023-07-03 22:08:43 +0200

    fix bias in linear net

--- linearlayer.py
+++ linearlayer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from lossfunctions import MSE
 from module import Module
+from optimizer import SGD
 from sequential import Sequential
 
 
@@ -10,7 +11,7 @@ class LinearLayer(Module):
         # self.weights = np.random.normal(0, 1, size=(input_neurons, output_neurons))
         # self.bias = np.random.normal(0, 1, size=(input_neurons, output_neurons))
         self.weights = np.ones((output_neurons, input_neurons))
-        self.bias = np.ones((output_neurons))
+        self.bias = np.ones((1, output_neurons))
 
         self.gradient_weights = None
         self.gradient_bias = None
@@ -25,8 +26,12 @@ class LinearLayer(Module):
         return np.dot(x, self.weights.T) + self.bias
 
     def backward(self, previous_gradient):
-        self.gradient_weights = previous_gradient * self.input_data
+        self.gradient_weights = previous_gradient.T * self.input_data # TODO why is it previous_gradient.T ?
         self.gradient_bias = previous_gradient
+
+        # print(self.gradient_weights, self.weights)
+        # print(self.gradient_bias, self.bias)
+
         return previous_gradient @ self.weights
 
     def get_parameters(self):
@@ -45,7 +50,7 @@ class LinearLayer(Module):
 if __name__ == "__main__":
     loss_function = MSE()
     network = Sequential([LinearLayer(1, 2), LinearLayer(2, 1)])
-
+    optimizer = SGD(lr=0.001)
     input = np.array([[1]])
     target = np.array([[1]])
     output = network.forward(input)
@@ -54,5 +59,12 @@ if __name__ == "__main__":
     mse_derivative = loss_function.backward(output, target)
 
     linear_gradient = network.backward(mse_derivative)
+
+    optimizer.step(network)
+    # I want to test if it actually goes down in the right direction
+    # Then I want to do nonlinear activation functions
+    # Lastly I want to do batches
+    # Then I want to run an end to end test,
+    # probably plotting the decision boundary and the 2D data points and the evolution over training
     print(linear_gradient)
     print(output)
